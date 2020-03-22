e8c12d9ccbe48df9ddb24ad5e950f21e78d357c8
sidoroval163 <alex.sid.92@mail.ru>
2020-03-22 14:09:10 +0300

    init:comleted task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let checkStr = bracketsConfig[i].join("");
+
+        if (str.includes(checkStr)) {
+            str = str.replace(checkStr, "");
+            i = -1;
+        }
+    }
+    return str ? false : true;
+};
